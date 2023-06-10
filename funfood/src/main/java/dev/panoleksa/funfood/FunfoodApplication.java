e79a22d60f93be51edb17df4fd743e7458bdf6e9
Oleksandr Panchuk <olexandrpan4uk@gmail.com>
2023-06-10 14:03:43 +0300

    init project

---
+++ funfood/src/main/java/dev/panoleksa/funfood/FunfoodApplication.java
@@ -0,0 +1,13 @@
+package dev.panoleksa.funfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunfoodApplication.class, args);
+	}
+
+}
---
+++ funfood/src/test/java/dev/panoleksa/funfood/FunfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.panoleksa.funfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
